d3839aab5d9b910446c646f4d695be0807868383
Denis Savishchev <denis.savishchev@gmail.com>
2024-11-30 15:02:18 +0100

    Initial Commit

---
+++ Screens/ScreensApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreensApp.swift
+//  Screens
+//
+//  Created by Devis on 30/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScreensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
