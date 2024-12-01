368a6a351f9b812bd5ad4bd876213475a3fea33f
Denis Savishchev <denis.savishchev@gmail.com>
2024-12-01 21:23:02 +0100

    bottom view

--- Screens/GuitarView/BottomView.swift
+++ Screens/GuitarView/BottomView.swift
@@ -1,16 +1,30 @@
 import SwiftUI
 
+
 struct BottomView: View {
     
-    private let names = ["Body", "Neck", "Pickups", "Equaliser", "Strings", "Manual"]
+    private let icons = [
+        Bottom(name: "Head", icon: "Head"),
+        Bottom(name: "EQ", icon: "EQ"),
+        Bottom(name: "Sound", icon: "Drum"),
+        Bottom(name: "Settings", icon: "Settings"),
+        Bottom(name: "Vinil", icon: "Vinil"),
+        Bottom(name: "Manual", icon: "Note")
+    ]
+    
+    @State private var isFavorite: Bool = false
     
     var body: some View {
-        VStack{
             ScrollView(.horizontal, showsIndicators: false){
                 HStack{
-                    ForEach(names, id: \.self){i in
-                        VStack{
-                            Text(i)
+                    ForEach(icons, id: \.id){icon in
+                        VStack(spacing: 0){
+                            Image(icon.icon)
+                                .shadow(color: .white, radius: 5)
+                            Text(icon.name)
+                                .shadow(color: .brown.opacity(0.5), radius: 5)
+                                .bold()
+                            Spacer()
                         }
                         .frame(width: 100, height: 100)
                         .background((LinearGradient(gradient: Gradient(
@@ -23,15 +37,22 @@ struct BottomView: View {
                                 .frame(width: 100, height: 100)
                         }
                         .shadow(color: .white.opacity(0.35), radius: 1)
+                        
                     }
                 }
                 .frame(maxWidth: .infinity)
-                .frame(height: 180)
-                .padding(.bottom, 40)
+                .frame(height: 100)
+                .padding(.bottom, 70)
+                .padding(.top, 2)
                 .padding(.horizontal, 18)
             }
-        }
-        
     }
 }
+
+struct Bottom: Identifiable{
+    let id = UUID()
+    let name: String
+    let icon: String
+}
+
 
