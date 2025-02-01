ed6f2b8264c800e3a7fd16bda98fdf7a911ab711
Denis Savishchev <denis.savishchev@gmail.com>
2025-02-01 21:37:01 +0100

    bike view

---
+++ Screens/BikeView/BikeView.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+struct BikeView: View {
+    var body: some View {
+        VStack{
+            ZStack(alignment: .top){
+                    Text("D5")
+                        .font(.system(size: 250, weight: .bold, design: .default))
+                        .foregroundColor(.bikeLightGray)
+                        .frame(height: 160)
+                
+                Rectangle()
+                    .frame(width: .infinity, height: 200)
+                    .foregroundStyle(LinearGradient(gradient: Gradient(stops: [
+                                    .init(color: .clear, location: 0.0),
+                                    .init(color: .bikeGray.opacity(0.5), location: 0.3),
+                                    .init(color: .bikeGray, location: 0.6),
+                                    .init(color: .bikeGray, location: 1.0) ]),
+                                                    startPoint: .top, endPoint: .bottom))
+                
+                Image("Bike")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.horizontal, 12)
+                
+                Text("SIMPLY. BETTER.")
+                    .font(.system(size: 20, weight: .bold, design: .default))
+                    .foregroundColor(.steamWhite.opacity(0.6))
+                    .rotationEffect(.degrees(90))
+                    .frame(width: 200, height: 16)
+                    .offset(x: 175, y: 65)
+            }
+
+                Spacer()
+        }
+        .background(.bikeGray)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        
+    }
+}
+
+#Preview {
+    BikeView()
+}
