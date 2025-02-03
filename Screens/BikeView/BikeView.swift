f2395330ad79065e4a3c43a38e7fc7301a75cbdd
Denis Savishchev <denis.savishchev@gmail.com>
2025-02-03 21:55:25 +0100

    bottom menu start

--- Screens/BikeView/BikeView.swift
+++ Screens/BikeView/BikeView.swift
@@ -7,7 +7,39 @@ struct BikeView: View {
             
             BikeUnitView()
 
-            Spacer()
+            ZStack(alignment:. bottom){
+                UnevenRoundedRectangle(cornerRadii: .init(
+                    topLeading: 0,
+                    bottomLeading: 30,
+                    bottomTrailing: 0,
+                    topTrailing: 30
+                ))
+                .foregroundStyle(LinearGradient(gradient: Gradient(colors: [.bikeLightBlue, .bikeBlue]), startPoint: .leading, endPoint: .trailing))
+                .shadow(color: .black.opacity(0.5), radius: 10, x: 10, y: 10)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                
+                UnevenRoundedRectangle(cornerRadii: .init(
+                    topLeading: 0,
+                    bottomLeading: 30,
+                    bottomTrailing: 0,
+                    topTrailing: 30
+                ))
+                .foregroundColor(.bikeLightGray)
+                .frame(maxWidth: .infinity, maxHeight: 400)
+                
+                UnevenRoundedRectangle(cornerRadii: .init(
+                    topLeading: 0,
+                    bottomLeading: 10,
+                    bottomTrailing: 0,
+                    topTrailing: 10
+                ))
+                .foregroundStyle(LinearGradient(gradient: Gradient(colors: [.bikeLightBlue, .bikeBlue]), startPoint: .leading, endPoint: .trailing))
+                .frame(maxWidth: 200, maxHeight: 60)
+                .offset(x: 80, y: 20)
+            }
+            .padding(.horizontal, 32)
+            .padding(.bottom, 24)
+           
         }
         .background(.bikeGray)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
