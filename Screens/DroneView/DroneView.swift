fbd109a55647f1e6ceead9f07a065bac4fca7ed0
Denis Savishchev <denis.savishchev@gmail.com>
2025-02-15 09:52:44 +0100

    chart

--- Screens/DroneView/DroneView.swift
+++ Screens/DroneView/DroneView.swift
@@ -13,6 +13,8 @@ struct DroneView: View {
                 
                 CurveContainer()
                     .foregroundColor(.ampWhite.opacity(0.05))
+                
+                BatteryView()
             }
             .frame(maxWidth: .infinity, maxHeight: 200)
 
@@ -32,3 +34,60 @@ struct DroneView: View {
     DroneView()
 }
 
+
+struct BatteryView: View {
+    var body: some View {
+        HStack{
+            BatteryStatusView()
+            
+            BatteryChartView()
+        }
+    }
+}
+
+struct BatteryStatusView: View {
+    var body: some View {
+        VStack(alignment: .leading){
+            Text("Battery Status")
+                .font(.system(size: 18, weight: .bold, design: .rounded))
+                .foregroundColor(.ampWhite)
+                .padding(.bottom, 12)
+            HStack(alignment: .bottom){
+                Text("37.9")
+                    .font(.system(size: 28, weight: .bold, design: .rounded))
+                    .foregroundColor(.ampWhite)
+                Text("\u{00B0}C")
+                    .font(.system(size: 18, weight: .bold, design: .rounded))
+                    .foregroundColor(.ampWhite.opacity(0.7))
+            }
+            Text("11 min ago")
+                .font(.system(size: 14, weight: .bold, design: .rounded))
+                .foregroundColor(.ampWhite.opacity(0.7))
+        }
+        .padding(.top, 70)
+    }
+}
+
+struct BatteryChartView: View {
+    
+    let randomNumbers = [3, 4, 5, 7, 2, 4, 6, 3, 10, 9, 7, 5, 7, 9]
+    
+    var body: some View {
+        ZStack{
+            HStack(alignment: .bottom){
+                ForEach(randomNumbers, id: \.self){i in
+                    VStack(spacing: 6){
+                        ForEach(0..<i, id: \.self){j in
+                                RoundedRectangle(cornerRadius: 1)
+                                .frame(width: 6, height: 6)
+                                .foregroundColor(j >= i - 2 ? .droneYellow.opacity(0.2) : .droneYellow)
+                        }
+                    }
+                }
+            }
+        }
+        .frame(width: 200, height: 150)
+        .background(.ampWhite.opacity(0.1))
+        .padding(.top, 20)
+    }
+}
