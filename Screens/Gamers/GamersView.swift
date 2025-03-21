51a535c296634bbbfd169551e6941d692eab9150
Denis Savishchev <denis.savishchev@gmail.com>
2025-03-21 06:16:12 +0100

    robot start

--- Screens/Gamers/GamersView.swift
+++ Screens/Gamers/GamersView.swift
@@ -4,6 +4,9 @@ struct GamersView: View {
     var body: some View {
         VStack {
             PrinceView()
+            Spacer()
+                .frame(maxHeight: 50)
+            RobotView()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(.black)
@@ -12,4 +15,30 @@ struct GamersView: View {
 
 #Preview {
     GamersView()
+}
+
+struct RobotView: View {
+    var body: some View {
+        ZStack{
+            VStack{
+                
+            }
+            .frame(maxWidth: .infinity, maxHeight: 320)
+            .background(LinearGradient(colors: [.retroBlack, .bikeGray], startPoint: .topLeading, endPoint: .bottomTrailing))
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(LinearGradient(colors: [.icyWhite.opacity(0.5), .icyWhite.opacity(0.1)], startPoint: .topLeading, endPoint: .bottomTrailing), lineWidth: 1)
+                .frame(maxWidth: UIScreen.main.bounds.width - 14, maxHeight: 320)
+            HStack {
+                Spacer()
+                Image("Robot")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 380)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: 350)
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .padding(.leading, 12)
+    }
 }
