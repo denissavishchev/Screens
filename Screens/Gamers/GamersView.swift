ee0f918dc1eb24343350121f405b2bce38b79fdc
Denis Savishchev <denis.savishchev@gmail.com>
2025-03-21 17:48:35 +0100

    robot basic ui

--- Screens/Gamers/GamersView.swift
+++ Screens/Gamers/GamersView.swift
@@ -20,12 +20,31 @@ struct GamersView: View {
 struct RobotView: View {
     var body: some View {
         ZStack{
-            VStack{
-                
+            VStack(alignment: .leading, spacing: 0){
+                Text("Copper robot")
+                    .font(.system(size: 28, weight: .bold, design: .rounded))
+                    .foregroundColor(.icyWhite)
+                Text("The Copper Robot is a sleek, futuristic automaton with a metallic bronze finish, designed for both utility and style")
+                    .font(.system(size: 12, weight: .bold, design: .rounded))
+                    .foregroundColor(.icyWhite.opacity(0.6))
+                    .shadow(color: .icyWhite, radius: 5)
+                    .padding(.trailing, 150)
+                    .padding(.bottom, 12)
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 220, height: 40)
+                    .foregroundColor(.white)
+                    .padding(.bottom, 12)
+                Text("Abillity")
+                    .font(.system(size: 22, weight: .bold, design: .rounded))
+                    .foregroundColor(.icyWhite.opacity(0.8))
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 220, height: 120)
+                    .foregroundColor(.white)
             }
-            .frame(maxWidth: .infinity, maxHeight: 320)
+            .frame(maxWidth: .infinity, maxHeight: 320, alignment: .leading)
             .background(LinearGradient(colors: [.retroBlack, .bikeGray], startPoint: .topLeading, endPoint: .bottomTrailing))
             .clipShape(RoundedRectangle(cornerRadius: 12))
+            .padding(.leading, 18)
             RoundedRectangle(cornerRadius: 12)
                 .stroke(LinearGradient(colors: [.icyWhite.opacity(0.5), .icyWhite.opacity(0.1)], startPoint: .topLeading, endPoint: .bottomTrailing), lineWidth: 1)
                 .frame(maxWidth: UIScreen.main.bounds.width - 14, maxHeight: 320)
