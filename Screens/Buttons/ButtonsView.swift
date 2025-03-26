3a80d1f22830d24469cc3d641a2e9aa68b27c1a3
Denis Savishchev <denis.savishchev@gmail.com>
2025-03-26 21:48:19 +0100

    heart animation

--- Screens/Buttons/ButtonsView.swift
+++ Screens/Buttons/ButtonsView.swift
@@ -53,7 +53,24 @@ struct ButtonsView: View {
                     background: transactionState.color,
                     symbolImage: transactionState.image
                 )
+                ZStack{
+                    Heart()
+                        .stroke(.white, style: StrokeStyle(lineWidth: 3))
+                        .phaseAnimator([1, 0]){fire, draw in
+                            ZStack{
+                                fire
+                                Heart()
+                                    .trim(from: 0, to: draw)
+                                    
+                            }
+                        }
+                        animation: {draw in
+                                .easeInOut(duration: 1)
+                        }
+                        .frame(width: 60, height: 50)
+                }
                 SnakeButton()
+                    .padding(.top, 50)
                 LazySwitch()
                     .padding(.top, 50)
                 AnimatedButton(config: config){
@@ -78,5 +95,34 @@ struct ButtonsView: View {
     ButtonsView()
 }
 
-
-
+struct Heart: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let width = rect.width
+        let height = rect.height
+        
+        path.move(to: CGPoint(x: width / 2, y: height))
+        
+        path.addCurve(to: CGPoint(x: 0, y: height / 3),
+                      control1: CGPoint(x: width / 2, y: height / 1.5),
+                      control2: CGPoint(x: 0, y: height / 2))
+        
+        path.addArc(center: CGPoint(x: width / 4, y: height / 4),
+                    radius: width / 4,
+                    startAngle: .degrees(180),
+                    endAngle: .degrees(0),
+                    clockwise: false)
+        
+        path.addArc(center: CGPoint(x: 3 * width / 4, y: height / 4),
+                    radius: width / 4,
+                    startAngle: .degrees(180),
+                    endAngle: .degrees(0),
+                    clockwise: false)
+        
+        path.addCurve(to: CGPoint(x: width / 2, y: height),
+                      control1: CGPoint(x: width, y: height / 2),
+                      control2: CGPoint(x: width / 2, y: height / 1.5))
+        
+        return path
+    }
+}
