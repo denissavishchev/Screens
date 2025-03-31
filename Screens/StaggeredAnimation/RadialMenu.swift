e7d752a3e776f9e2b2a00a5aeee6bfe08925641e
Denis Savishchev <denis.savishchev@gmail.com>
2025-03-31 17:14:02 +0200

    radial menu done

--- Screens/StaggeredAnimation/RadialMenu.swift
+++ Screens/StaggeredAnimation/RadialMenu.swift
@@ -12,6 +12,12 @@ struct RadialMenu: View {
     @State private var selectedIndex: Int = 0
     @State private var isAnimating: Bool = false
     @State private var animatingIndex: CGFloat = 1
+    func icons(_ index: Int) -> String {
+        let icon = ["photo.fill", "music.note.house", "book.closed.fill", "mappin.circle", "video", "message.fill"]
+        return icon[index % icon.count]
+    }
+    @State private var selectedIcon: String = "photo.fill"
+    
     
     var body: some View {
         ZStack{
@@ -36,34 +42,65 @@ struct RadialMenu: View {
                 let angle = Double(i) / Double(6) * 2 * .pi
                 let radius: CGFloat = 95
                 
-                RoundedTrapezoid()
-                    .frame(width: 100, height: 100)
-                    .foregroundColor(.black)
-                    .rotationEffect(.degrees(-90 + Double(i * 60)))
-                    .offset(
-                        x: radius * cos(angle),
-                        y: radius * sin(angle)
-                    )
-                    .onTapGesture {
-                        if !isAnimating {
-                            animateSelection(from: selectedIndex, to: i)
+                ZStack {
+                    RoundedTrapezoid()
+                        .frame(width: 100, height: 100)
+                        .foregroundStyle(LinearGradient(colors: [.gray, .black.opacity(0.7)], startPoint: .bottom, endPoint: .top))
+                        .rotationEffect(.degrees(-90 + Double(i * 60)))
+                        .offset(
+                            x: radius * cos(angle),
+                            y: radius * sin(angle)
+                        )
+                        .onTapGesture {
+                            if !isAnimating {
+                                animateSelection(from: selectedIndex, to: i)
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                                    withAnimation {
+                                        selectedIcon = icons(i)
+                                    }
+                                }
+                            }
                         }
-                    }
                     .shadow(color: .black.opacity(0.5), radius: 4, x: 2, y: 2)
+                    Image(systemName: icons(i))
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(.white)
+                        .offset(
+                            x: (radius + 20) * cos(angle),
+                            y: (radius + 20) * sin(angle)
+                        )
+                        .shadow(color: .black, radius: 1.5)
+                        .shadow(color: .black.opacity(0.8), radius: 10, x: 5, y: 5)
+                }
             }
             if selectedIndex >= 0 {
                 let angle = Double(selectedIndex) / Double(6) * 2 * .pi
                 let radius: CGFloat = 95
                     
-                RoundedTrapezoid()
-                    .frame(width: 100, height: 100)
-                        .foregroundColor(.white)
-                        .rotationEffect(.degrees(-90 + Double(selectedIndex * 60)))
-                        .offset(
-                            x: radius * cos(angle),
-                            y: radius * sin(angle)
-                        )
+                ZStack {
+                    RoundedTrapezoid()
+                        .frame(width: 100, height: 100)
+                        .foregroundStyle(LinearGradient(colors: [.orange, .orange, .orange, .red], startPoint: .top, endPoint: .bottom))
+                            .rotationEffect(.degrees(-90 + Double(selectedIndex * 60)))
+                            .offset(
+                                x: radius * cos(angle),
+                                y: radius * sin(angle)
+                            )
                         .shadow(color: .black.opacity(0.5), radius: 4, x: 2, y: 2)
+                    Image(systemName: selectedIcon)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(.white)
+                        .offset(
+                            x: (radius + 20) * cos(angle),
+                            y: (radius + 20) * sin(angle)
+                        )
+                        .shadow(color: .black, radius: 1.5)
+                        .shadow(color: .black.opacity(0.8), radius: 10, x: 5, y: 5)
+                }
             }
             ZStack {
                 Circle()
