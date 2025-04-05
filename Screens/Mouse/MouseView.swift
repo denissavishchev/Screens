7f9b12bd4d17a79c5c7376a93fe014c1f4ee478a
Denis Savishchev <denis.savishchev@gmail.com>
2025-04-05 19:48:51 +0200

    bottom line shapes

--- Screens/Mouse/MouseView.swift
+++ Screens/Mouse/MouseView.swift
@@ -24,6 +24,19 @@ struct MouseView: View {
                 .offset(x: 150, y: 350)
                 .foregroundColor(.mouseGreen)
                 .shadow(color: .mouseGreen, radius: 3)
+            TopLinesShape()
+                .stroke(.mouseGreen, lineWidth: 3)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .shadow(color: .mouseGreen, radius: 5)
+                .padding(18)
+            BottomLinesShape()
+                .stroke(
+                    .mouseGreen,
+                    style: StrokeStyle(lineWidth: 5, lineCap: .round)
+                )
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .shadow(color: .mouseGreen, radius: 8)
+                .padding(18)
                 
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -36,6 +49,52 @@ struct MouseView: View {
     MouseView()
 }
 
+struct TopLinesShape: Shape{
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.maxX - 32, y: rect.minY + 4))
+        path.addLine(to: CGPoint(x: rect.maxX - 60, y: rect.minY + 30))
+        path.addLine(to: CGPoint(x: rect.maxX - 80, y: rect.minY + 30))
+        path.addLine(to: CGPoint(x: rect.maxX - 100, y: rect.minY + 45))
+        path.addEllipse(in: CGRect(x: rect.maxX - 103, y: rect.minY + 43, width: 5, height: 5))
+
+        path.move(to: CGPoint(x: rect.maxX - 27, y: rect.minY + 8))
+        path.addLine(to: CGPoint(x: rect.maxX - 57, y: rect.minY + 37))
+        path.addLine(to: CGPoint(x: rect.maxX - 58, y: rect.minY + 130))
+        path.addLine(to: CGPoint(x: rect.maxX - 80, y: rect.minY + 150))
+        path.addEllipse(in: CGRect(x: rect.maxX - 84, y: rect.minY + 149, width: 5, height: 5))
+        
+        path.move(to: CGPoint(x: rect.maxX - 22, y: rect.minY + 11))
+        path.addLine(to: CGPoint(x: rect.maxX - 50, y: rect.minY + 39))
+        path.addLine(to: CGPoint(x: rect.maxX - 50, y: rect.minY + 180))
+        path.addEllipse(in: CGRect(x: rect.maxX - 52, y: rect.minY + 180, width: 5, height: 5))
+       return path
+    }
+}
+
+struct BottomLinesShape: Shape{
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.minX, y: rect.maxY - 12))
+        path.addLine(to: CGPoint(x: rect.minX + 40, y: rect.maxY - 40))
+        path.addLine(to: CGPoint(x: rect.minX + 40, y: rect.maxY - 180))
+        path.addEllipse(in: CGRect(x: rect.minX + 37, y: rect.maxY - 185, width: 5, height: 5))
+
+//        path.move(to: CGPoint(x: rect.maxX - 27, y: rect.minY + 8))
+//        path.addLine(to: CGPoint(x: rect.maxX - 57, y: rect.minY + 37))
+//        path.addLine(to: CGPoint(x: rect.maxX - 58, y: rect.minY + 130))
+//        path.addLine(to: CGPoint(x: rect.maxX - 80, y: rect.minY + 150))
+//        path.addEllipse(in: CGRect(x: rect.maxX - 84, y: rect.minY + 149, width: 5, height: 5))
+//        
+//        path.move(to: CGPoint(x: rect.maxX - 22, y: rect.minY + 11))
+//        path.addLine(to: CGPoint(x: rect.maxX - 50, y: rect.minY + 39))
+//        path.addLine(to: CGPoint(x: rect.maxX - 50, y: rect.minY + 180))
+//        path.addEllipse(in: CGRect(x: rect.maxX - 52, y: rect.minY + 180, width: 5, height: 5))
+       return path
+    }
+}
 
 
 struct MouseOverlay: View {
@@ -44,6 +103,7 @@ struct MouseOverlay: View {
             Text("Mouse")
                 .font(.system(size: 90, weight: .bold, design: .monospaced))
                 .foregroundStyle(LinearGradient(colors: [.gray, .gray.opacity(0.5)], startPoint: .leading, endPoint: .trailing))
+            
             Spacer()
         }
     }
