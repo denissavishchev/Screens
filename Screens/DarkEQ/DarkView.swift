dbba061789ce3783b2666c6af7a8c1cd26bd7653
Denis Savishchev <denis.savishchev@gmail.com>
2025-04-12 21:17:58 +0200

    rotation slider

--- Screens/DarkEQ/DarkView.swift
+++ Screens/DarkEQ/DarkView.swift
@@ -4,8 +4,12 @@ struct DarkView: View {
     
     @State private var selected: Int = 0
     
+    
     var body: some View {
         VStack{
+            VolumeView()
+            Spacer()
+                .frame(height: 60)
             SlidersView()
             Spacer()
                 .frame(height: 20)
@@ -33,4 +37,61 @@ struct DarkView: View {
 
 #Preview {
     DarkView()
+}
+
+struct VolumeView: View {
+    
+    @State var volume: Int = 0
+    @State var angle: Double = 0
+    
+    var body: some View {
+        ZStack{
+            ForEach(0...50, id: \.self) { i in
+                let angle = Double(i) / Double(50) * 360
+                let radius: CGFloat = 120
+                
+                ZStack {
+                    RoundedRectangle(cornerRadius: 2)
+                        .frame(width: 3, height: 18)
+                        .foregroundColor(i < volume / 2 + 1 ? .turco : .gray.opacity(0.1))
+                    RoundedRectangle(cornerRadius: 2)
+                        .stroke(i < volume / 2 + 1 ? .black : .black.opacity(0.3), lineWidth: 0.5)
+                        .frame(width: 4, height: 20)
+                }
+                .offset(y: -radius)
+                .rotationEffect(.degrees(angle))
+            }
+            ZStack {
+                Circle()
+                    .frame(width: 200, height: 200)
+                    .foregroundColor(.red)
+//                Circle()
+//                    .frame(width: 20, height: 20)
+//                    .foregroundColor(.blue)
+//                    .offset(y: -80)
+//                    .rotationEffect(.degrees(angle * .pi))
+                VStack {
+                    Text("\(volume)")
+                    Text("\(angle)")
+                }
+                    
+            }
+            .gesture(
+                DragGesture()
+                    .onChanged(onDrag(value:))
+            )
+            
+        }
+        .frame(height: 180)
+    }
+    func onDrag(value: DragGesture.Value){
+        let vector = CGVector(dx: value.location.x, dy: value.location.y)
+        let radians = atan2(vector.dy - 10, vector.dx - 10)
+        var angle  = radians * 180 / .pi
+        if angle < 0{angle = 360 + angle}
+        withAnimation(Animation.linear(duration: 0.3)){
+            self.angle = Double(angle)
+            self.volume = Int(angle)
+        }
+    }
 }
